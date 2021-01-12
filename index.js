0f8418f26ab36a3c1e4b629501143067ff366c05
Seb <covarrubias.seb@gmail.com>
2021-01-11 22:02:02 -0800

    index.html

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let express = require('express');
 
 let app = new express();
 
-app.listen(3000, ()=>{
+app.use(express.static(__dirname + "/public"));
+
+app.listen(3000, () => {
 	console.log("Listening on Port 3000");
 });
