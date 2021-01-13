d2f02d9db8fd74cdf60b08382a7ed5a265bf6789
Seb <covarrubias.seb@gmail.com>
2021-01-12 22:53:24 -0800

    Add comments

--- index.js
+++ index.js
@@ -8,25 +8,47 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 // Handle Login
 app.post("/api/login", (req, res) => {
+	``
+	// check if username exists in db
+		// if not, return err
+
+	// check password with bcrypt
+		// if match, return user data from db
+		// if not, return err
+
 	console.log(req.body.username);
 	console.log(req.body.password);
 });
 
 // Handle Sign Up
 app.post("/api/signup", (req, res) => {
+
+	// check if username exists in db
+		// if so, return err
+
+	// create new user in db with:
+	// username, salt, hash, tasks
+
 	console.log(req.body.username);
 	console.log(req.body.password);
 });
 
+
+// Handle Adding Tasks
 app.post("/api/add", (req, res) => {
 	// data is a string
 	res.send(req.body.data);
+
+	// add task to db
 });
 
+// Handle Completed Tasks
 app.post("/api/complete", (req, res) => {
 	// data is an array of objects with id, value keys
 	//  id is the client side id for the task's element
 	res.json(req.body.data);
+
+	// remove tasks from db
 });
 
 app.listen(port, () => {
