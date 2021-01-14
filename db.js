557ea4e49539bb680e3e547d397027c266049997
Seb <covarrubias.seb@gmail.com>
2021-01-13 16:49:12 -0800

    Salt does not need to be stored

--- db.js
+++ db.js
@@ -5,8 +5,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceN
 
 let userSchema = new mongoose.Schema({
 	username: {type: String, required: true},
-	hash: {type: String, required: true},
-	salt: {type: String, required: true}
+	hash: {type: String, required: true}
 });
 
 let taskSchema = new mongoose.Schema({
